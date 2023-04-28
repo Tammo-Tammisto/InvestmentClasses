99e665efb10ef6bb089356c7fe683ff1a2841690
Tammo-Tammisto <113598068+Tammo-Tammisto@users.noreply.github.com>
2023-04-28 11:41:17 +0300

    Update TransactionsLoader.cs

--- InvestmentClasses/Data/InMemoryData/TransactionsLoader.cs
+++ InvestmentClasses/Data/InMemoryData/TransactionsLoader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using InvestmentClasses.Domain;
 
@@ -9,6 +9,9 @@ namespace InvestmentClasses.Data.InMemoryData
         public static void LoadTransactions(DataContext context)
         {
             Pohikonto20230401(context); // Põhikonto rahade liikumine 01.04.2023
+            LHVArvelduskonto345645633(context);
+            LHVKasvukonto236554211(context);
+            LHVVäärtpaberikonto973647626(context);
         }
 
         private static void Pohikonto20230401(DataContext context)
@@ -72,6 +75,105 @@ namespace InvestmentClasses.Data.InMemoryData
             });
 
             swed.Transactions.AddRange(context.Transactions.Where(t => t.OwningAccount == swed));
+        }
+        
+        private static void LHVArvelduskonto345645633(DataContext context)
+        {
+            var lhv = context.Accounts.GetByName("LHV arvelduskonto");
+            var eur = context.Securables.GetByTicker("EUR");
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 10:00:00"),
+                Amount = 1000m,
+                Securable = eur,
+                Description = "Raha arvelduseks"
+            });
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 14:00:00"),
+                Amount = -0.5m,
+                Securable = eur,
+                Description = "Konto hooldustasu"
+            });
+            lhv.Transactions.AddRange(context.Transactions.Where(t => t.OwningAccount == lhv));
+        }
+
+        private static void LHVKasvukonto236554211(DataContext context)
+
+        {
+            var lhv = context.Accounts.GetByName("LHV kasvukonto");
+            var eur = context.Securables.GetByTicker("EUR");
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 10:00:00"),
+                Amount = 100m,
+                Securable = eur,
+                Description = "Raha kasvamiseks"
+            });
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 14:00:00"),
+                Amount = -0.5m,
+                Securable = eur,
+                Description = "Konto hooldustasu"
+            });
+            lhv.Transactions.AddRange(context.Transactions.Where(t => t.OwningAccount == lhv));
+        }
+
+        private static void LHVVäärtpaberikonto973647626(DataContext context)
+        {
+            var lhv = context.Accounts.GetByName("LHV väärtpaberikonto");
+            var eur = context.Securables.GetByTicker("EUR");
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 10:00:00"),
+                Amount = 100m,
+                Securable = eur,
+                Description = "Raha väärtpaberiteks"
+            });
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 11:00:00"),
+                Amount = -50m,
+                Securable = eur,
+                Description = "Tesla väärtpaberid"
+            });
+
+            context.Transactions.Add(new Transaction
+            {
+                TransactionId = "",
+                OwningAccount = lhv,
+                OtherAccount = null,
+                Time = DateTime.Parse("2023-04-01 14:00:00"),
+                Amount = -0.5m,
+                Securable = eur,
+                Description = "Konto hooldustasu"
+            });
+            lhv.Transactions.AddRange(context.Transactions.Where(t => t.OwningAccount == lhv));
         }
     }
 }
